a3ea3e5b3d97bc5df28c3836d732ad487e17cf69
rifatrahman1 <ahsan30rifat@gmail.com>
2025-01-01 10:18:29 +0600

    added to local storage specific id

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
+import { add_to_LS, get_stored_cart } from "../../Utilities/local_storage";
 
 const Bottles = () => {
     const [bottles, set_bottles] = useState([]);
@@ -8,13 +9,32 @@ const Bottles = () => {
 
     useEffect(() => {
         fetch('bottle.json')
-        .then(res => res.json())
-        .then(data => set_bottles(data))
+            .then(res => res.json())
+            .then(data => set_bottles(data))
     }, []);
-    
+
+    useEffect(() => {
+        if (bottles.length > 0) {
+            const stored_cart = get_stored_cart();
+            console.log(stored_cart, bottles);
+            const saved_cart = [];
+            
+            for (const id of stored_cart) {
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if (bottle) {
+                    saved_cart.push(bottle)
+                }
+            }
+            console.log('save cart', saved_cart);
+            set_cart(saved_cart);
+        }
+    }, [bottles])
+
     const handle_add_to_cart = (bottle) => {
         const new_cart = [...cart, bottle];
         set_cart(new_cart);
+        add_to_LS(bottle.id)
     }
 
 
@@ -22,11 +42,11 @@ const Bottles = () => {
         <div>
             <h3>Cart: {cart.length}</h3>
             <div className="bottles">
-            
-            {
-                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handle_add_to_cart={handle_add_to_cart}></Bottle>)
-            }
-        </div>
+
+                {
+                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handle_add_to_cart={handle_add_to_cart}></Bottle>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Utilities/local_storage.js
@@ -0,0 +1,24 @@
+
+
+const get_stored_cart = () => {
+    const stored_cart = localStorage.getItem('cart');
+    if (stored_cart) {
+        return JSON.parse(stored_cart);
+    }
+    else {
+        return [];
+    }
+}
+
+const save_cart_LS = (cart) => {
+    const cart_stringify = JSON.stringify(cart);
+    localStorage.setItem('cart', cart_stringify)
+}
+
+const add_to_LS = (id) => {
+    const cart = get_stored_cart();
+    cart.push(id);
+    save_cart_LS(cart);
+}
+
+export {add_to_LS, get_stored_cart};
